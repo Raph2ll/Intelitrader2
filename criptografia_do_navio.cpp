ea3babab65e1b35fef2a158dcc3f967c7f308d57
Raph2ll <raphael.ph2ll@gmail.com>
2024-06-08 11:44:31 -0300

    feat: criptografia_do_navio

--- criptografia_do_navio.cpp
+++ criptografia_do_navio.cpp
@@ -3,20 +3,36 @@
 #include <bitset>
 #include <stdexcept>
 
-std::string binaryToAscii(const std::string &binaryStr) {
-    std::string asciiStr;
+using namespace std;
+
+char swapHalfBytes(char byte)
+{
+    return ((byte & 0xF0) >> 4) | ((byte & 0x0F) << 4);
+}
+
+char swapLastTwoBits(char byte)
+{
+    return byte ^ 0b11;
+}
+
+string binaryToAscii(const string &binaryStr)
+{
+    string asciiStr;
     int bitCount = 0;
     char chr = 0;
 
-    for (char bit : binaryStr) {
+    for (char bit : binaryStr)
+    {
         chr <<= 1;
-        if (bit == '1') {
+        if (bit == '1')
+        {
             chr |= 1;
         }
 
         ++bitCount;
-        if (bitCount == 8) {
-            asciiStr += chr;
+        if (bitCount == 8)
+        {
+            asciiStr += swapHalfBytes(swapLastTwoBits(chr));
             bitCount = 0;
             chr = 0;
         }
@@ -30,17 +46,10 @@ int main(int argc, char *argv[])
 
     for (int i = 1; i < argc; ++i)
     {
-        std::string binaryStr = argv[i];
-        try
-        {
-            std::string asciiStr = binaryToAscii(binaryStr);
-            std::cout << asciiStr;
-        }
-        catch (const std::exception &e)
-        {
-            std::cerr << "Please enter a valid value" << std::endl;
-            return 1;
-        }
+        string binaryStr = argv[i];
+
+        string asciiStr = binaryToAscii(binaryStr);
+        cout << asciiStr;
     }
 
     return 0;
